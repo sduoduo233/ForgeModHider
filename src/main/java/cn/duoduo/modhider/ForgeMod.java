ca64f151867645190b747f5b008742b7fb72bdc4
duoduo <sduoduo233@gmail.com>
2022-03-18 09:09:46 +0800

    version 1.1

--- src/main/java/cn/duoduo/modhider/ForgeMod.java
+++ src/main/java/cn/duoduo/modhider/ForgeMod.java
@@ -3,7 +3,7 @@ package cn.duoduo.modhider;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = "modhider", name = "ForgeModHider", version = "1.0", guiFactory = "cn.duoduo.modhider.config.GuiFactory")
+@Mod(modid = "modhider", name = "ForgeModHider", version = "1.1", guiFactory = "cn.duoduo.modhider.config.GuiFactory")
 public class ForgeMod {
     @Mod.EventHandler
     public void onFMLInitialization(FMLInitializationEvent event) {
